6cc464cc81e9b90c09611f41d1fef674345e571f
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-08-26 00:22:43 -0400

    error

--- backend/config/constants.js
+++
@@ -1,8 +0,0 @@
-// config/constants.js
-const path = require("path");
-
-const EXCEL_DIR = path.join(__dirname, "..", "docs"); // '..' para subir un nivel desde config/
-
-module.exports = {
-  EXCEL_DIR,
-};
